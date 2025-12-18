d6faad8b885f1d56795bf80c1cf1b6dec8c963ae
caralikas <caralikas@students.berkeley.net>
2025-12-18 04:58:44 +0000

    codespace 6

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-function string_bits(str){
-    let result = "";
-    for (let i = 0; i<str.length; i+= 2){
-        result += str[i];
+function caughtSpeeding(speed,isBirthday){
+    if (isBirthday){
+        speed -=5;
+    } 
+    if (speed <=60){
+        return 0;
+    } else if (speed<=80){
+        return 1;
+    } else {
+        return 2;
     }
-    return result;
-}
+
+    }
