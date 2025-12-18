08cc36ecdfb307e73375bfa712ba8ec27618bc80
caralikas <caralikas@students.berkeley.net>
2025-12-18 05:57:52 +0000

    codespace 9

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-function specialEleven(n){
-    return n%11===0 || n%11===1
+function withoutDoubles(die1,die2, noDoubles){
+    if (noDoubles && die1===die2){
+        die1=die1===6?1:die1+1;
+    }
+    return die1+die2;
     }
