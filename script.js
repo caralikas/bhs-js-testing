e92fa15b42c311adaaee8e5471888cc5db342284
caralikas <caralikas@students.berkeley.net>
2025-12-18 06:02:32 +0000

    codespace 10

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-function withoutDoubles(die1,die2, noDoubles){
-    if (noDoubles && die1===die2){
-        die1=die1===6?1:die1+1;
-    }
-    return die1+die2;
+function left2(str){
+    let result = str.substring(0,2);
+    let end = str.substring(2);
+    return end + result;
     }
