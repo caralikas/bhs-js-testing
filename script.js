b59cbc922fd9988762c92ef29bd070667e3fd4df
caralikas <caralikas@students.berkeley.net>
2025-12-18 04:30:52 +0000

    codespace 1

--- script.js
+++ script.js
@@ -3,6 +3,6 @@
  * Run after you make changes to re-run the tests with your new code.
  */
 
-function monkey_trouble(a_smile,b_smile){
-    return a_smile == b_smile;
+function sleep_in(weekday,vacation){
+    return !weekday || vacation;
 }
