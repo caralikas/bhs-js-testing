cc9d48b55dd46bb2a5ef6a06c43b298d2d8b05ad
caralikas <caralikas@students.berkeley.net>
2025-12-18 04:36:07 +0000

    codespace 1

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-/*
- * This file is where you should write your code. Remember to click
- * Run after you make changes to re-run the tests with your new code.
- */
-
 function sleep_in(weekday,vacation){
     return !weekday || vacation;
 }
